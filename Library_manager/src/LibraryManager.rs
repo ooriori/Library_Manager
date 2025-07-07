bba9733ef5e62c91ab3751cbb35bfc1050fe028d
ooriori <senyassrcruzr@gmail.com>
2025-07-07 04:35:18 +0200

    Update LibraryManager.rs

--- Library_manager/src/LibraryManager.rs
+++ Library_manager/src/LibraryManager.rs
@@ -1,6 +1,6 @@
-#[derive(Debug)]
-struct Book {
-    title: String,
+#[derive(Debug)]  //This is an attribute that tells Rust to automatically generate code so we can print the book with println!("{:?}", book). Very useful for debugging.
+struct Book {   ////Here you define a structure called Book, which groups 3 pieces of information:
+    title: String, 
     author: String,
     is_borrowed: bool,
 }
